c7ff04ce6cdd2a3095bd0b93273856515b7ae631
Ahmed5720 <AhmedNassar5720@gmail.com>
2024-03-21 08:11:02 -0400

    cleaned code, added seperate class for camera and interval

---
+++ hittable.h
@@ -0,0 +1,38 @@
+//
+// Created by 15854 on 3/14/2024.
+//
+
+#ifndef RAYTRACER_HITTABLE_H
+#define RAYTRACER_HITTABLE_H
+
+#include "ray.h"
+
+class hit_record{
+
+public:
+    point3 p;
+    vec3 normal;
+    double t;
+    bool front_face;
+
+    void set_normal_dir(const ray& r, const vec3& outnorm)
+    {
+        // we define a normal to face front if the direction of the ray is opposite the direction of the surface normal
+        // if a ray comes from outside
+        front_face = dot(r.direction(), outnorm) < 0;
+        if(front_face)
+            normal = outnorm;
+        else
+            normal = -outnorm;
+    }
+};
+
+
+class hittable {
+public:
+    virtual ~hittable() = default;
+    // const = 0, pure virtual, needs implementation
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+
+};
+#endif //RAYTRACER_HITTABLE_H
---
+++ sphere.h
@@ -0,0 +1,54 @@
+//
+// Created by 15854 on 3/14/2024.
+//
+
+#ifndef RAYTRACER_SPHERE_H
+#define RAYTRACER_SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+// inherits from hittable
+class sphere : public hittable {
+    public:
+    sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        vec3 ac = r.origin() - center;
+        auto a = dot(r.direction(), r.direction());
+        auto b = 2.0 * dot(r.direction(), ac);
+        auto c = dot(ac, ac) - radius * radius;
+        auto discriminant = b * b - 4 * a * c;
+
+        // to get the normal we return the solution for t
+        if (discriminant < 0)
+            return false;
+
+        // find nearest root to tmax and tmin
+        auto root = (-b - sqrt(discriminant)) / (2 * a);
+        if(root <= ray_tmin || ray_tmax <= root)
+        {
+            root = (-b + sqrt(discriminant)) / (2 * a);
+            if(root <= ray_tmin || ray_tmax <= root)
+            {
+                return false;
+            }
+        }
+
+
+        rec.t = root;
+        rec.p = r.at(root);
+        rec.normal = (rec.p - center) / radius;
+        // for a sphere we can obtain a normal by getting the hitpoint - the center, we can normalize by dividing by r
+        vec3 outnorm = (rec.p - center) / radius;
+        rec.set_normal_dir(r,outnorm);
+        return true;
+
+    }
+
+private:
+    point3 center;
+    double radius;
+};
+
+#endif //RAYTRACER_SPHERE_H
